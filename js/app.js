03479aeba1c94c8f1f25e1666f8ab877b9602d02
record001-gitlab <umarkhidirboev@gmail.com>
2022-03-11 19:37:31 +0500

    courses2

--- js/app.js
+++ js/app.js
@@ -76,3 +76,50 @@ facts.addEventListener("mouseenter", () => {
   const obj3 = document.getElementById("value3");
   animateValue(obj3, 0, 70, 3000);
 });
+
+// courses modal
+
+let course__close_btn = document.querySelector(".course__close_btn");
+let courses__modal = document.querySelector(".courses__modal");
+let courses__card = document.querySelectorAll(".courses__card");
+let courses__modal__right = document.querySelector(".courses__modal__right")
+let courses__carousel  =document.querySelector(".courses__carousel")
+
+course__close_btn.addEventListener("click", () => {
+  courses__modal.classList.remove("d_block");
+  courses__modal.classList.add("d_none");
+});
+
+courses__card.forEach((card) => {
+  card.addEventListener("click", (event) => {
+
+    courses__modal.classList.remove("d_none");
+    courses__modal.classList.add("d_block");
+
+    let id = event.target.dataset.id
+    // console.log(event.target.dataset.id);
+
+    let card = course.filter(item => id == item.id)
+
+    console.log(card[0].title);
+
+    courses__modal__right.innerHTML = `
+    <h2>${card[0].title}</h2>
+    <p>${card[0].description}.</p>
+    <p>${card[0].cost}</p>    `
+   
+    courses__carousel.innerHTML = `
+    <div class="carousel-item active">
+    <img class="d-block w-100" src=${card[0].img[0]}  alt="First slide">
+  </div>
+  <div class="carousel-item">
+    <img class="d-block w-100" src=${card[0].img[1]}  alt="Second slide">
+  </div>
+  <div class="carousel-item">
+    <img class="d-block w-100" src=${card[0].img[2]} alt="Third slide">
+  </div>
+    `
+  });
+});
+
+
---
+++ js/course.js
@@ -0,0 +1,67 @@
+const course = [
+  {
+    id: 1,
+    title: "Fortepiano",
+    description:"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempora quod mollitia aperiam cumque doloribus, ipsum    eligendi optio earum officia ut labore commodi esse rerum illum! Nesciunt illum temporibus error repellendus totam    laboriosam voluptates porro, ea quas molestias accusamus quae, praesentium, suscipit consequatur quibusdam illo    deleniti saepe tempora odit."   ,
+    img: ["./img/lesson/f1.jpg", "./img/lesson/f3.jpg", "./img/lesson/f4.jpg"],
+    cost: "kurs narxi 80 000 so'm"
+
+  },
+  {
+    id: 2,
+    title: "Xalq cholg'ulari",
+    description:"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempora quod mollitia aperiam cumque doloribus, ipsum    eligendi optio earum officia ut labore commodi esse rerum illum! Nesciunt illum temporibus error repellendus totam    laboriosam voluptates porro, ea quas molestias accusamus quae, praesentium, suscipit consequatur quibusdam illo    deleniti saepe tempora odit."   ,
+    img: ["./img/lesson/xch1.jpg", "./img/lesson/xch2.jpg", "./img/lesson/xch3.jpg"],
+    cost: "kurs narxi 80 000 so'm"
+
+  },
+  {
+    id: 3,
+    title: "Teatr san'ati",
+    description:"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempora quod mollitia aperiam cumque doloribus, ipsum    eligendi optio earum officia ut labore commodi esse rerum illum! Nesciunt illum temporibus error repellendus totam    laboriosam voluptates porro, ea quas molestias accusamus quae, praesentium, suscipit consequatur quibusdam illo    deleniti saepe tempora odit."   ,
+    img: ["./img/lesson/t1.jpg", "./img/lesson/t2.jpg", "./img/lesson/t3.jpg"],
+    cost: "kurs narxi 80 000 so'm"
+
+  },
+  {
+    id: 4,
+    title: "Estrada xonandaligi",
+    description:"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempora quod mollitia aperiam cumque doloribus, ipsum    eligendi optio earum officia ut labore commodi esse rerum illum! Nesciunt illum temporibus error repellendus totam    laboriosam voluptates porro, ea quas molestias accusamus quae, praesentium, suscipit consequatur quibusdam illo    deleniti saepe tempora odit."   ,
+    img: ["./img/lesson/ex1.jpg", "./img/lesson/ex2.jpg", "./img/lesson/es1.jpg"],
+    cost: "kurs narxi 80 000 so'm"
+
+  },
+  {
+    id: 5,
+    title: "Tasviriy san`at",
+    description:"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempora quod mollitia aperiam cumque doloribus, ipsum    eligendi optio earum officia ut labore commodi esse rerum illum! Nesciunt illum temporibus error repellendus totam    laboriosam voluptates porro, ea quas molestias accusamus quae, praesentium, suscipit consequatur quibusdam illo    deleniti saepe tempora odit."   ,
+    img: ["./img/lesson/art2.jpg","./img/lesson/art4.jpg","./img/lesson/art5.jpg"],
+    cost: "kurs narxi 80 000 so'm"
+
+  },
+  {
+    id: 6 ,
+    title: "Amaliy san`at",
+    description:"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempora quod mollitia aperiam cumque doloribus, ipsum    eligendi optio earum officia ut labore commodi esse rerum illum! Nesciunt illum temporibus error repellendus totam    laboriosam voluptates porro, ea quas molestias accusamus quae, praesentium, suscipit consequatur quibusdam illo    deleniti saepe tempora odit."   ,
+    img: ["./img/lesson/as2.jpg", "./img/lesson/as3.jpg","./img/lesson/as5.jpg"],
+    cost: "kurs narxi 80 000 so'm"
+
+  },
+  {
+    id: 7 ,
+    title: "Akademik xonandalik",
+    description:"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempora quod mollitia aperiam cumque doloribus, ipsum    eligendi optio earum officia ut labore commodi esse rerum illum! Nesciunt illum temporibus error repellendus totam    laboriosam voluptates porro, ea quas molestias accusamus quae, praesentium, suscipit consequatur quibusdam illo    deleniti saepe tempora odit."   ,
+    img: ["./img/lesson/ax1.jpg", "./img/lesson/ax2.jpg", "./img/lesson/ax3.jpg"],
+    cost: "kurs narxi 80 000 so'm"
+
+  },
+  {
+    id: 8,
+    title: "Estrada cholg`u ijrochiligi",
+    description:"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempora quod mollitia aperiam cumque doloribus, ipsum    eligendi optio earum officia ut labore commodi esse rerum illum! Nesciunt illum temporibus error repellendus totam    laboriosam voluptates porro, ea quas molestias accusamus quae, praesentium, suscipit consequatur quibusdam illo    deleniti saepe tempora odit."   ,
+    img: ["./img/lesson/xch4.jpg", "./img/lesson/echi1.jpg", "./img/lesson/ex2.jpg"],
+    cost: "kurs narxi 80 000 so'm"
+
+  },
+
+]
